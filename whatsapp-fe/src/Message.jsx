e9e3fdd887831bade2a8a460e0e96ed1e1193b19
Giuseppe Farano <giuseppe.farano02@gmail.com>
2023-06-09 18:05:35 +0200

    fe_sent/received_fixed

--- whatsapp-fe/src/Message.jsx
+++ whatsapp-fe/src/Message.jsx
@@ -3,11 +3,11 @@ import './style/Message.css'
 
 function Message(props) {
     return (
-        <p className={ Boolean(props.sent) ? 'chat_message' : 'chat_message chat_receiver'}>
+        <p className={ props.sent ? 'chat_message ' : 'chat_receiver'}>
                 <span className='chat_name'>{props.author}</span>
                 {props.content}
                 <span className='chat_timestamp'>{new Date().toUTCString()}</span>
-            </p>
+        </p>
     )
 }
 
