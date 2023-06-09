9502947f428967e092325607c01cac9b539632dd
Giuseppe Farano <giuseppe.farano02@gmail.com>
2023-06-09 13:05:25 +0200

    front-end

--- whatsapp-fe/src/Chat.jsx
+++ whatsapp-fe/src/Chat.jsx
@@ -4,6 +4,8 @@ import { Avatar, IconButton } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import MicIcon from '@mui/icons-material/Mic';
 
 function Chat() {
   return (
@@ -32,6 +34,26 @@ function Chat() {
                 Sono gay
                 <span className='chat_timestamp'>{new Date().toUTCString()}</span>
             </p>
+
+            <p className='chat_message chat_receiver'>
+                <span className='chat_name'>Simone</span>
+                Vero
+                <span className='chat_timestamp'>{new Date().toUTCString()}</span>
+            </p>
+        </div>
+
+        <div className='chat_footer'>
+            <InsertEmoticonIcon />
+            <form>
+                <input
+                placeholder="Scrivi un messaggio"
+                type="text"
+                />
+                <button type="submit">
+                    Invia
+                </button>
+            </form>
+            <MicIcon />
         </div>
     </div>
   )
