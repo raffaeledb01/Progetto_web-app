fcb479ff294a5f31f1d075cd6d94ada2386cd19d
raffaeledb01 <dibiraffaele2001@gmail.com>
2023-06-10 22:16:23 +0200

    aggiunta e modifica dei models

--- whatsapp-be/models/chats.js
+++ whatsapp-be/models/chats.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 const chatSchema = mongoose.Schema({
-    
+    partecipants: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    messages: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message"
+    }]
 });
 
 mongoose.exports = mongoose.model('Chat', chatSchema);
--- whatsapp-be/models/users.js
+++ whatsapp-be/models/users.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    
+    email: String,
+    password: String,
+    chats: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Chat"
+    }]
 });
 
 mongoose.exports = mongoose.model('User', userSchema);
