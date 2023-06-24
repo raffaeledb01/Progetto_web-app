8bc53685c7985633509622e073d36e7859e16d0c
raffaeledb01 <dibiraffaele2001@gmail.com>
2023-06-24 18:35:05 +0200

    inzio sign up

--- whatsapp-fe/src/Login.jsx
+++ whatsapp-fe/src/Login.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
